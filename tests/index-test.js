6867ee29187864f3a975ba4cc112ce9e8b1e7ffd
TarikHuber <huber.tarik@gmail.com>
2017-03-15 11:32:08 +0100

    Refactored tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -4,23 +4,4 @@ import {render, unmountComponentAtNode} from 'react-dom'
 
 import {ResponsiveDrawer} from 'src/'
 
-describe('ResponsiveDrawer', () => {
-
-  /*
-  let node
-
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
-
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('contains drawer', () => {
-    render(<ResponsiveDrawer/>, node, () => {
-      expect(node.innerHTML).toContain('Drawer')
-    })
-  })
-  */
-})
+expect(ResponsiveDrawer).toExist()
