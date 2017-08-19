b111c8169411115a8ab9f3de3cad82a1dd5866b8
tarikhuber <huber.tarik@gmail.com>
2017-08-19 22:54:26 +0200

    Added tests

---
+++ src/store/reducer.spec.js
@@ -0,0 +1,49 @@
+import expect from 'expect'
+import reducer from './reducer'
+import * as actions from './actions'
+
+const initialState = {
+	docked: false,
+	responsive: true,
+	open: false,
+	searching: false,
+}
+
+describe('locale reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual(initialState)
+  })
+
+  it('should not affect state', () => {
+    expect(
+      reducer(initialState, {type: 'NOT_EXISTING'})
+    ).toEqual(initialState)
+  })
+
+  it('should handle toggleDrawerOpen', () => {
+    expect(
+      reducer(initialState, actions.toggleDrawerOpen(true))
+    ).toEqual({...initialState, open: true})
+  })
+
+  it('should handle toggleDrawerDock', () => {
+    expect(
+      reducer(initialState, actions.toggleDrawerDock(true))
+    ).toEqual({...initialState, docked: true})
+  })
+
+  it('should handle setDrawerOpen', () => {
+    expect(
+      reducer(initialState, actions.setDrawerOpen(true))
+    ).toEqual({...initialState, open: true})
+  })
+
+  it('should handle setResponsive', () => {
+    expect(
+      reducer(initialState, actions.setResponsive(true))
+    ).toEqual({...initialState, responsive: true})
+  })
+
+})
--- tests/index-test.js
+++
@@ -1,7 +0,0 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
-
-import {ResponsiveDrawer} from 'src/'
-
-expect(ResponsiveDrawer).toExist()
